d6198f3b1efc942067e2d22bcd0219bc4dbd0fd9
Tyler Buchanan <t.buchananx97@gmail.com>
2018-04-14 14:02:23 -0400

    Implemented most of the ServerProvider to use the wrapper for the TcpListener

--- Orion/Orion.Server.Tests/TestServerProvider.cs
+++ Orion/Orion.Server.Tests/TestServerProvider.cs
@@ -1,23 +1,84 @@
 ﻿namespace Orion.Server.Tests
 {
+    using NSubstitute;
+
     using NUnit.Framework;
 
+    using Orion.Logger.Abstract;
     using Orion.Server.Concrete;
+    using Orion.Server.Wrapper.Abstract;
 
     [TestFixture]
     public class TestServerProvider
     {
+        private const string ReadyForConnectionMessage = "Reading for connections...";
+
+        private const string StartUpMessage = "Starting Server...";
+
+        private IOrionLogger orionLoggerMock;
+
         private ServerProvider systemUnderTest;
+
+        private ITcpListenerWrapper tcpListenerWrapperMock;
+
+        [Test]
+        public void RunServer_WhenInvoked_LoggerMockLogMessageIsCalledWithReadyForConnectionMessage()
+        {
+            InvokeRunServer();
+
+            orionLoggerMock.Received(1).LogMessage(ReadyForConnectionMessage);
+        }
+
+        [Test]
+        public void RunServer_WhenInvoked_LoggerMockLogMessageIsCalledWithStartUpMessage()
+        {
+            InvokeRunServer();
+
+            orionLoggerMock.Received(1).LogMessage(StartUpMessage);
+        }
+
+        [Test]
+        public void RunServer_WhenInvoked_TcpListenerWrapperMockStartIsCalled()
+        {
+            InvokeRunServer();
+
+            tcpListenerWrapperMock.Received(1).Start();
+        }
+
+        [Test]
+        public void RunServer_WhenInvoked_TcpListnerWrapperMockAcceptTcpClientIsCalled()
+        {
+            InvokeRunServer();
+
+            tcpListenerWrapperMock.Received(1).AcceptTcpClient();
+        }
 
         [SetUp]
         public void Setup()
         {
+            tcpListenerWrapperMock = CreateTcpListenerWrapperMock();
+            orionLoggerMock = CreateOrionLoggerMock();
             systemUnderTest = CreateSystemUnderTest();
+        }
+
+        private IOrionLogger CreateOrionLoggerMock()
+        {
+            return Substitute.For<IOrionLogger>();
         }
 
         private ServerProvider CreateSystemUnderTest()
         {
-            return new ServerProvider();
+            return new ServerProvider(orionLoggerMock, tcpListenerWrapperMock);
+        }
+
+        private ITcpListenerWrapper CreateTcpListenerWrapperMock()
+        {
+            return Substitute.For<ITcpListenerWrapper>();
+        }
+
+        private void InvokeRunServer()
+        {
+            systemUnderTest.RunServer();
         }
     }
 }
--- Orion/Orion.Server/Concrete/ServerProvider.cs
+++ Orion/Orion.Server/Concrete/ServerProvider.cs
@@ -1,14 +1,39 @@
 ﻿namespace Orion.Server.Concrete
 {
-    using System;
-
+    using Orion.Logger.Abstract;
     using Orion.Server.Abstract;
+    using Orion.Server.Wrapper.Abstract;
 
     public class ServerProvider : IServerProvider
     {
+        private const string ReadyForConnectionMessage = "Reading for connections...";
+
+        private const string StartUpMessage = "Starting Server...";
+
+        private readonly IOrionLogger orionLogger;
+
+        private readonly ITcpListenerWrapper tcpListenerWrapper;
+
+        public ServerProvider(IOrionLogger orionLogger, ITcpListenerWrapper tcpListenerWrapper)
+        {
+            this.orionLogger = orionLogger;
+            this.tcpListenerWrapper = tcpListenerWrapper;
+        }
+
+        public bool ServerRunning { get; set; } = true; //TODO: Setup an actual controlled flag for this.
+
         public void RunServer()
         {
-            throw new NotImplementedException();
+            orionLogger.LogMessage(StartUpMessage);
+            tcpListenerWrapper.Start();
+
+            while (ServerRunning)
+            {
+                orionLogger.LogMessage(ReadyForConnectionMessage);
+                tcpListenerWrapper.AcceptTcpClient();
+
+                ServerRunning = false; //Only to run test once. (For now until implementation is complete)
+            }
         }
     }
 }
